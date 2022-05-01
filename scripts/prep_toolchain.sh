6dd178b405db7e93b2da2b5cb63a31c31fc26742
truhy <78481693+truhy@users.noreply.github.com>
2022-05-01 11:30:34 +0100

    prep_toolchain.sh to aarch32

--- scripts/prep_toolchain.sh
+++ scripts/prep_toolchain.sh
@@ -13,7 +13,10 @@ cd ~
 #   Target = which system the elf or binary will be running on. E.g. if the development kit is running linux then target is linux, or if no OS then target is baremetal
 #   x86 Linux = 32bits Linux on Intel processor
 #   x64 Linux = 64bits Linux on Intel processor
-#   AArch64 Linux = 64bits Linux on Arm processor
+#   AArch32 = 32bit Arm processor
+#   AArch64 = 64bit Arm processor
+#   AArch32 Linux = Linux on 32bit Arm processor
+#   AArch64 Linux = Linux on 64bit Arm processor
 #   Windows (mingw-w64-i686) = 64bits Windows on Intel processor and mingw-w64-i686 C/C++ compiler
 #   Hard float = This refers to the target Arm processor which includes a hardware floating unit (ALU) and this option will make use of it. For Linux target the std C/C++ (e.g. glibc) library needs be precompiled so enabling hard float is not selectable using just a switch.  For baremetal a switch can enable the hard float
 
@@ -29,7 +32,7 @@ cd ~
 # https://github.com/xpack-dev-tools/arm-none-eabi-gcc-xpack/releases/
 
 #FILE=gcc-arm-10.3-2021.07-x86_64-arm-none-linux-gnueabihf.tar.xz
-FILE=gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf.tar.xz
+FILE=gcc-arm-10.3-2021.07-x86_64-arm-none-eabi.tar.xz
 URL=https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-2021.07/binrel
 
 if [ ! -f "$FILE" ]; then
