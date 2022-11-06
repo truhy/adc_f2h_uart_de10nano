027fc17927481f52bcbd4a49b66a810f42c6f9ec
truhy <78481693+truhy@users.noreply.github.com>
2022-11-06 19:41:02 +0000

    added test_bench

---
+++ test_bench/run_modelsim.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/intelFPGA/20.1/modelsim_ase/bin
+./vsim
+
---
+++ test_bench/run_questa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Setup env variables
+export QUESTA_BIN=~/intelFPGA_lite/21.1/questa_fse/bin
+export LM_LICENSE_FILE=~/LR-068117_License.dat
+
+$QUESTA_BIN/vsim
+
